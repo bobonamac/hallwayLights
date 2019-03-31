70a5afb64d8409cefad9553610bde79c6ca8ed08
bobonamac <bobviens@gmail.com>
2019-03-31 11:50:14 +0200

    first commitski

---
+++ hallwayLights.c
@@ -0,0 +1,83 @@
+// calculates the Car Talk puzzler of which light is left on in the hallway
+
+#include <stdio.h>
+
+/* 
+	- prompt for a number of lightbulbs and store as numBulbs
+	- create bool array with numBulbs elements set to off
+	- print bulbs
+	- prompt for "press go"
+	- loop cntr with numBulbsPasses w/ nested numBulbs cntr to flip elements
+	- print after each loop
+	- print element numbers left on
+*/
+
+// **************************************************************************80
+
+/* 
+makes a block of size prompted from user 
+*/
+
+#include<stdbool.h>
+#include<stdio.h>
+#include<string.h>
+
+// prototypes
+int promptPosInt();
+void printHallway(bool hallwayBulbs[], int bulbs);
+
+/***********************************************/
+
+int main(void) {
+	int bulbs = promptPosInt();
+	bool hallwayBulbs[bulbs];
+
+	for (int i = 0; i < bulbs; ++i)
+	{
+		hallwayBulbs[i] = false;
+	}
+
+	printHallway(hallwayBulbs, bulbs);
+
+	return 0;
+}
+
+/***********************************************/
+
+int promptPosInt() {
+	int n;
+	do {
+		printf("\nHow many lightbulbs? ");
+		scanf("%d", &n);
+	}
+	while (n < 1);
+
+	return n;
+}
+
+/***********************************************/
+
+void printHallway(bool hallwayBulbs[], int bulbs) {
+	printf("\n");
+	// using bulbs + 1 to prevent 'Floating Point Exception' error
+	for (int runCount = 1; runCount < bulbs + 1; runCount++) {
+
+		// print each of the elements
+		for (int elementCount = 0; elementCount < bulbs; elementCount++) {
+			printf("%s ", hallwayBulbs[elementCount] ? "o" : "_");
+		}
+		// space between runs
+		printf("\n");
+
+		// when runCount is 5 zB. if the flipCount is divisible by runCount, flip value
+		// using bulbs + 1 to prevent 'Floating Point Exception' error
+		// flip all the appropriate switches
+		for (int flipCount = 0; flipCount < bulbs; flipCount++) {
+			if (flipCount % runCount == 0) {
+			hallwayBulbs[flipCount] = !hallwayBulbs[flipCount];	
+			}	
+		}
+	}
+	printf("\n");
+	return;
+}

bb57a734cdfdaf5e6b6bf557603ae8b0d7eb9204
bobonamac <bobviens@gmail.com>
2019-04-02 21:23:25 +0200

    working and ready for review

--- hallwayLights.c
+++ hallwayLights.c
@@ -1,37 +1,45 @@
-// calculates the Car Talk puzzler of which light is left on in the hallway
+// calculates the Car Talk puzzler - which lights are left on in the hallway
 
 #include<stdbool.h>
 #include<stdio.h>
 #include<string.h>
 
-// prototypes
+// function prototypes
 int promptPosInt();
-void flipAndPrint(bool hallwayBulbs[], int bulbs);
+void printHallway(bool hallwayBulbs[], int bulbs);
+void pullChains(bool hallwayBulbs[], int bulbs);
+void bulbsLeftOn(bool hallwayBulbs[], int bulbs);
 
 /***********************************************/
 
 int main(void) {
+	// prompt for number of lightbulbs in the hallway
 	int bulbs = promptPosInt();
-	bool hallwayBulbs[bulbs];
 
+	// create and then initialize hallwayBulb array to "off" 
+	bool hallwayBulbs[bulbs];
 	for (int i = 0; i < bulbs; ++i)
 	{
 		hallwayBulbs[i] = false;
 	}
+
+	// space after prompt for number of bulbs
 	printf("\n");
 
 	// print the hallway with all the lights off
-	for (int j = 0; j < bulbs; j++) {
-		printf("%s ", hallwayBulbs[j] ? "o" : "_");
-	}
-	printf("\n");
-	// execute the chain pulls, print, and list bulbs left on
-	flipAndPrint(hallwayBulbs, bulbs);
+	printHallway(hallwayBulbs, bulbs);
+
+	// execute the chain pulls
+	pullChains(hallwayBulbs, bulbs);
+
+	// print a list of bulbs left on
+	bulbsLeftOn(hallwayBulbs, bulbs);
 
 	return 0;
 }
 
-/***********************************************/
+/*********************************************************************************************************************/
+/*********************************************************************************************************************/
 
 int promptPosInt() {
 	int n;
@@ -46,34 +54,54 @@ int promptPosInt() {
 
 /***********************************************/
 
-void flipAndPrint(bool hallwayBulbs[], int bulbs) {
-	printf("\n");
-	// do this bulb number of times
+void printHallway(bool hallwayBulbs[], int bulbs) {
+
+	// print a pictographic representation of the hallwayBulb array
+	for (int j = 0; j < bulbs; j++) {
+		printf("%s ", hallwayBulbs[j] ? "o" : "_");
+	}
+	// space between lines
+	printf("\n\n");
+
+	return;
+}
+
+/***********************************************/
+
+void pullChains(bool hallwayBulbs[], int bulbs) {
+
+	// do this "bulb" number of times
 	for (int everyNthChain = 1; everyNthChain < bulbs + 1; everyNthChain++) {
 
-		// start with Nth chain and increment by the same till bulbs is reached
+		// start with Nth chain and increment by the same till "bulbs" is reached
 		for (int i = everyNthChain; i <  bulbs + 1; i += everyNthChain) {
 			hallwayBulbs[i - 1] = !hallwayBulbs[i - 1];	
 		}	
-		// print each of the elements
-		for (int j = 0; j < bulbs; j++) {
-			printf("%s ", hallwayBulbs[j] ? "o" : "_");
-		}
-		// space between runs
-		printf("\n\n");
+		
+		// print a pictographic representation of the hallwayBulb array
+		printHallway(hallwayBulbs, bulbs);
 	}
+	
+	return;
+}
+
+/***********************************************/
+
+void bulbsLeftOn(bool hallwayBulbs[], int bulbs) {
+	
 	// print list of bulbs left on
 	printf("Bulbs left on: ");
+
 	for (int k = 0; k < bulbs; k++) {
+
+		// if the bulb is "on", print the element number + 1 = bulb number
 		if (hallwayBulbs[k] == true) {
-			printf("%d", k + 1);	
-		}
-		if (k != bulbs - 1 && hallwayBulbs[k] == true) {
-				printf(", ");
+			printf("%d", k + 1);
+			printf(", ");	
 		}
 	}
 
-	printf("\n\n\n");
+	printf("and that's all.\n\n\n");
 
 	return;
 }
